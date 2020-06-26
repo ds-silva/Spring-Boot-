fbb7d4143ccfee07cd187d6cd1b5637edf9cc305
David Barbosa da Silva <davidinteraction@outlook.com>
2020-06-26 16:07:58 -0300

    Bean Validation

---
+++ src/main/java/br/com/alura/forum/controller/Boot.java
@@ -0,0 +1,10 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.boot.SpringApplication;
+
+public class Boot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+}
