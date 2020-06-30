0d2a06ff02a1ac977f13dcdc364bf83fb80e0149
David Barbosa da Silva <davidinteraction@outlook.com>
2020-06-30 12:59:57 -0300

    Ordenação

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -42,9 +43,10 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
+			@RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
 		
-		PageRequest paginacao = PageRequest.of(pagina, qtd);
+		PageRequest paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
 		
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
